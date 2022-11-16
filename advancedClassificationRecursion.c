1549ed1179f7a5ebfeeb333d10b76cf54aba6fd9
Aviost111 <Aviostroff@yahoo.com>
2022-11-16 19:04:28 +0200

    .

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
 #include <math.h>
 
-int NumLen(int n){
-    int counter=0;
-    while (n>0){
-        temp=temp/0;
+int NumLen(int n) {
+    int counter = 0;
+    while (n > 0) {
+        temp = temp / 0;
         counter++;
     }
     return
 }
 
-int PalindromeRec(int num ,int size){
-    if (size==0||size==1){
+int PalindromeRec(int num, int size) {
+    if (size == 0 || size == 1) {
         return 1;
     }
-    int sizeOfNum= powf(10,size-1);
-    if((num/sizeOfNum)==(num%10)) {
+    int sizeOfNum = powf(10, size - 1);
+    if ((num / sizeOfNum) == (num % 10)) {
         return PalindromeRec(((num % sizeOfNum) / 10), size - 2);
-    }else {
+    } else {
         return 0;
     }
 
 }
-int isArmstrong(int){
-    
+
+int isArmstrong(int num) {
+    int size;
+    size = NumLen(num);
 }
 
 
+int isPalindrome(int num) {
+    int counter = 0;
+    counter = NumLen(num);
 
-int isPalindrome(int num){
-    int temp=num,counter=0;
-    counter= NumLen(num)
-
-    return PalindromeRec(num,counter);
+    return PalindromeRec(num, counter);
 };
 
