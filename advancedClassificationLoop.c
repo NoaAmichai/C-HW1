af610ed84148baea6190e1ee631e8d7695a00ba3
Noa Amichai <Noa1254@gmail.com>
2022-11-16 16:17:20 +0200

    .

--- advancedClassificationLoop.c
+++ advancedClassificationLoop.c
@@ -12,10 +12,19 @@ int isArmstrong(int n) {
 }
 
 
-int isPalindrome(int n) {
-	
-	
-	
-	
-}
 
+isPalindrome(int num){
+    int temp=num,numSum=0;
+
+    while (temp!=0){
+        numSum=numSum*10;
+        numSum+=temp%10;
+        temp=temp/10;
+    }
+    if(numSum==num){
+        return 1;
+    }
+    return 0;
+
+
+}
