7b4182778e1416c00de8f42a5792ed259c150018
Noa Amichai <Noa1254@gmail.com>
2022-11-16 19:32:02 +0200

    Update basicClassification.c

--- basicClassification.c
+++ basicClassification.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 #include <math.h>
 
-int isPrime(int num){
-    for(i=2;i<num;i++){
-        if (num%i==0){
-            return 0;
+int isPrime(int num) {
+    if (num < 1) { return 0; }
+    for (int i = 1; i < sqrt(num); i++) {
+        if (num % i == 0) {
+            return 1;
         }
     }
-    return 1;
+    return 0;
 }
 
-int isStrong(int num){
-    int endOfNum,powerSum=1,numSum=0,temp=num;
-    while(temp!=0){
-        endOfNum=temp%10;
-        for(i=1;i<=endOfNum;i++){
-            powerSum=powerSum*i;
+int isStrong(int num) {
+    int endOfNum, powerSum = 1, numSum = 0, temp = num;
+    while (temp != 0) {
+        endOfNum = temp % 10;
+        for (i = 1; i <= endOfNum; i++) {
+            powerSum = powerSum * i;
         }
-        numSum+=powerSum;
-        powerSum=1;
-        temp=temp/10;
+        numSum += powerSum;
+        powerSum = 1;
+        temp = temp / 10;
     }
-    if(num==numSum) {
+    if (num == numSum) {
         return 1;
     }
     return 0;
