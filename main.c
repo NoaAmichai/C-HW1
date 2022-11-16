d1dc4c12a79b4ea93769bd2eec5b28e8e86a31d9
Aviost111 <Aviostroff@yahoo.com>
2022-11-16 20:00:47 +0200

    main

--- main.c
+++ main.c
@@ -2,7 +2,31 @@
 #include "NumClass.h"
 #include <"math.h">
 
-int main(){
-    printf(powf(10,5));
+int main() {
+
+    int firstNum, secondNum, big, small;
+    scanf("%d%d", &firstNum, &secondNum);
+    big = fmax(firstNum, secondNum);
+    small = fmin(firstNum, secondNum);
+    for (int i = small; i <= big; i++) {
+        if (isPrime(i)) {
+            printf("%d ", i);
+        }
+    }
+    for (int i = small; i <= big; i++) {
+        if (isArmstrong(i)) {
+            printf("%d ", i);
+        }
+    }
+    for (int i = small; i <= big; i++) {
+        if (isStrong(i)) {
+            printf("%d ", i);
+        }
+    }
+    for (int i = small; i <= big; i++) {
+        if (isPalindrome(i)) {
+            printf("%d ", i);
+        }
+    }
     return 0;
 }
