12324d43d8041dac2bb90d58513c7fbca15d62d9
Noa Amichai <Noa1254@gmail.com>
2022-11-16 17:04:05 +0200

    Update advancedClassificationLoop.c

--- advancedClassificationLoop.c
+++ advancedClassificationLoop.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
+#include <math.h>
 
 int isArmstrong(int n) {
-    //String str = Integer.toString(n);
-    //int len = str.length();
-    int i, sum = 0;
-    while (n > 0) {
-        //sum += pow(n % 10, len);
-        n /= 10;
+    int sum = 0, temp = n, countLen = 0;
+    while (temp > 0) {
+        countLen++;
+        temp /= 10;
+    }
+    temp = n;
+    while (temp > 0) {
+        sum += pow(temp % 10, countLen);
+        temp /= 10;
     }
     return sum == n ? 1 : 0;
 }
 
 
+isPalindrome(int num) {
+    int temp = num, numSum = 0;
 
-isPalindrome(int num){
-    int temp=num,numSum=0;
-
-    while (temp!=0){
-        numSum=numSum*10;
-        numSum+=temp%10;
-        temp=temp/10;
+    while (temp != 0) {
+        numSum = numSum * 10;
+        numSum += temp % 10;
+        temp = temp / 10;
     }
-    if(numSum==num){
+    if (numSum == num) {
         return 1;
     }
     return 0;
